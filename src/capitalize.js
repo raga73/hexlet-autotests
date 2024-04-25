934483c3232010be8f801546a3788969b394f193
raga73 <a.nivas@yandex.ru>
2024-04-25 19:48:20 +0400

    upd

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,9 +1,8 @@
-export const capitalize = (text) => {
+export default function (text) {
     if (text === '') {
-      return '';
+        return '';
     }
     const firstChar = text[0].toUpperCase();
     const restSubstring = text.slice(1);
     return `${firstChar}${restSubstring}`;
-  };
-  
+};
